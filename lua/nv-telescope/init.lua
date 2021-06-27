e57acb4269b4d0b8ea480752c1428386037c372a
Ahmed Rezk <work72019@gmail.com>
2021-06-27 00:40:52 -0600

    Telescope key mapping.

--- lua/nv-telescope/init.lua
+++ lua/nv-telescope/init.lua
@@ -71,3 +71,28 @@ require('telescope').setup {
         }
     }
 }
+
+local opt = {noremap = true, silent = true}
+
+vim.g.mapleader = " "
+
+-- mappings
+vim.api.nvim_set_keymap("n", "<Leader>ff", [[<Cmd>lua require('telescope.builtin').find_files()<CR>]], opt)
+vim.api.nvim_set_keymap(
+    "n",
+    "<Leader>fp",
+    [[<Cmd>lua require('telescope').extensions.media_files.media_files()<CR>]],
+    opt
+)
+
+vim.api.nvim_set_keymap("n", "<Leader>fb", [[<Cmd>lua require('telescope.builtin').buffers()<CR>]], opt)
+vim.api.nvim_set_keymap("n", "<Leader>fh", [[<Cmd>lua require('telescope.builtin').help_tags()<CR>]], opt)
+vim.api.nvim_set_keymap("n", "<Leader>fo", [[<Cmd>lua require('telescope.builtin').oldfiles()<CR>]], opt)
+vim.api.nvim_set_keymap("n", "<Leader>fm", [[<Cmd> Neoformat<CR>]], opt)
+
+-- dashboard stuff
+vim.api.nvim_set_keymap("n", "<Leader>fw", [[<Cmd> Telescope live_grep<CR>]], opt)
+vim.api.nvim_set_keymap("n", "<Leader>fn", [[<Cmd> DashboardNewFile<CR>]], opt)
+vim.api.nvim_set_keymap("n", "<Leader>fb", [[<Cmd> DashboardJumpMarks<CR>]], opt)
+vim.api.nvim_set_keymap("n", "<Leader>fl", [[<Cmd> SessionLoad<CR>]], opt)
+vim.api.nvim_set_keymap("n", "<Leader>fs", [[<Cmd> SessionSave<CR>]], opt)
